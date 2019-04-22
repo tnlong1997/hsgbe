8fa6b9dcb1a7c192cb8965d0c288f1b79aa72680
Hoang Nguyen <hoangnguyenthai97@gmail.com>
2019-04-22 10:58:23 -0400

    Add Game, Team, Sport db; Add Game Controller and route

---
+++ hsgexpress/models/Team.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var teamSchema = new Schema({
+  host: {
+    type: Schema.Type.ObjectId,
+    ref: 'User'
+  },
+  participants: [
+    {
+      type: Schema.Type.ObjectId,
+      ref: 'User'
+    }
+  ]
+}, { timestamps: true });
+
+module.exports = mongoose.model('Team', teamSchema);
