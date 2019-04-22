6a7a5ffbf62779497354c0464b0d717528e204d3
Hoang Nguyen <hoangnguyenthai97@gmail.com>
2019-04-22 11:39:39 -0400

    Add middlewares for authorization; add implementation of creating games and listing games

--- hsgexpress/models/Game.js
+++ hsgexpress/models/Game.js
@@ -2,36 +2,36 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var gameSchema = new Schema({
-  host: {
-    type: Schema.Type.ObjectId,
-    ref: 'User'
-  },
-  participants: [
-    {
-      type: Schema.Type.ObjectId,
-      ref: 'User'
-    }
-  ],
-  sport: {
-    type: Schema.Type.ObjectId,
-    ref: 'Sport'
-  },
-  team: [
-    {
-      type: Schema.Type.ObjectId,
-      ref: 'Team'
-    }
-  ],
-  address: {
-    type: String,
-    require: [true, 'Location is required']
-  },
-  start_at: {
-    type: Date
-  },
-  end_at: {
-    type: Date
-  }
+	host: {
+		type: Schema.Types.ObjectId,
+		ref: 'User'
+	},
+	participants: [
+		{
+			type: Schema.Types.ObjectId,
+			ref: 'User'
+		}
+	],
+	sport: {
+		type: Schema.Types.ObjectId,
+		ref: 'Sport'
+	},
+	team: [
+		{
+			type: Schema.Types.ObjectId,
+			ref: 'Team'
+		}
+	],
+	address: {
+		type: String,
+		require: [true, 'Location is required']
+	},
+	start_at: {
+		type: Date
+	},
+	end_at: {
+		type: Date
+	}
 }, { timestamps: true });
 
 module.exports = mongoose.model('Game', gameSchema);
--- hsgexpress/models/Team.js
+++ hsgexpress/models/Team.js
@@ -2,16 +2,16 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var teamSchema = new Schema({
-  host: {
-    type: Schema.Type.ObjectId,
-    ref: 'User'
-  },
-  participants: [
-    {
-      type: Schema.Type.ObjectId,
-      ref: 'User'
-    }
-  ]
+	host: {
+		type: Schema.Type.ObjectId,
+		ref: 'User'
+	},
+	participants: [
+		{
+			type: Schema.Type.ObjectId,
+			ref: 'User'
+		}
+	]
 }, { timestamps: true });
 
 module.exports = mongoose.model('Team', teamSchema);
