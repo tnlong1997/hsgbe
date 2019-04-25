9fd7aebce96d0b49e3f628bf412948ae8cf382f0
Hoang Nguyen <hoangnguyenthai97@gmail.com>
2019-04-25 14:01:41 -0400

    Lint fix 2

--- hsgexpress/controllers/gameController.js
+++ hsgexpress/controllers/gameController.js
@@ -13,36 +13,36 @@ exports.game_list = function(req, res) {
 };
 
 exports.game_create = function(req, res) {
-  if(!req.body.participants || !req.body.team){
-    return res.send({status: 400, err: "Missing request requirements"});
-  }
+	if (!req.body.participants || !req.body.team) {
+		return res.send({status: 400, err: "Missing request requirements"});
+	}
 
 	let newGame = new Game({
-    host: req.decoded._id,
-    participants: req.body.participants.map(_p => mongoose.Types.ObjectId(_p)),
-    team: req.body.team.map(_t => mongoose.Types.ObjectId(_t)),
-    location: req.body.location
-  });
-  let idStr = req.body.participants;
+		host: req.decoded._id,
+		participants: req.body.participants.map(_p => mongoose.Types.ObjectId(_p)),
+		team: req.body.team.map(_t => mongoose.Types.ObjectId(_t)),
+		location: req.body.location
+	});
+	let idStr = req.body.participants;
 
-  Team.find({ '_id' : { $in : newGame.team }}, function(err, teams){
-    if(err) return res.send({ status: 500, err: err});
+	Team.find({ '_id': { $in: newGame.team }}, function(err, teams) {
+		if (err) return res.send({ status: 500, err: err});
 
-    teams.map(_team => {
-      _team.members.map(_p => {
-        if(!idStr.includes(_p.toString())){
-          newGame.participants.push(_p);
-          idStr.push(_p.toString());
-        }
-      });
-    });
+		teams.map(_team => {
+			_team.members.map(_p => {
+				if (!idStr.includes(_p.toString())) {
+					newGame.participants.push(_p);
+					idStr.push(_p.toString());
+				}
+			});
+		});
 
-    newGame.save(function(err) {
-      if(err) return res.send({status: 500, err: err});
+		newGame.save(function(err) {
+			if (err) return res.send({status: 500, err: err});
 
-      res.send({status: 200, newGame: newGame});
-    });
-  });
+			res.send({status: 200, newGame: newGame});
+		});
+	});
   
 };
 
