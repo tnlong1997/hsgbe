0100334052230a68656b603c5ae61893f8a539b3
tnlong1997 <tnlong1997@gmail.com>
2019-05-26 22:24:06 +0700

    API Doc

---
+++ hsgexpress/apidoc/index.js
@@ -0,0 +1,9 @@
+let express = require('express');
+let app = express();
+
+app.use(express.static('./'));
+app.get('/', (req, res) => {
+  res.sendFile('index.html');
+})
+console.log("Server connected");
+app.listen(3000)
